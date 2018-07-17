1574783088e5d9432f5d2232ca5e9a1da6db7727
LJaglin <ljaglinski@o2.pl>
2018-07-17 20:19:42 +0200

    Initial commit

---
+++ src/main/java/pl/ljaglin/springboothandlingformsubmission/SpringBootHandlingFormSubmissionApplication.java
@@ -0,0 +1,12 @@
+package pl.ljaglin.springboothandlingformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandlingFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHandlingFormSubmissionApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/ljaglin/springboothandlingformsubmission/SpringBootHandlingFormSubmissionApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ljaglin.springboothandlingformsubmission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHandlingFormSubmissionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
