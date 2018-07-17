8d1ac5ae48596fe1ef38891567b59d95e50d1e6d
LJaglin <ljaglinski@o2.pl>
2018-07-17 20:54:22 +0200

    Add a controller that below handles get and post requests

--- src/main/java/pl/ljaglin/springboothandlingformsubmission/controllers/FormController.java
+++ src/main/java/pl/ljaglin/springboothandlingformsubmission/controllers/FormController.java
@@ -1,15 +1,23 @@
 package pl.ljaglin.springboothandlingformsubmission.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.ljaglin.springboothandlingformsubmission.models.FamousPerson;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
-    @ResponseBody
-    public String sayHello() {
-        return "HELLO";
+    public String displayForm(Model model) {
+        model.addAttribute("famousPerson", new FamousPerson());
+        return "famousPersonForm";
+    }
+
+    @PostMapping("/form")
+    public String formSubmit(@ModelAttribute FamousPerson famousPerson) {
+        return "resultPage";
     }
 }
